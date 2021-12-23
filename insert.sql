7561f4e20e224a12014a70bf22cd989cd9db22d2
latifis <74188153+latifis@users.noreply.github.com>
2021-12-23 12:10:08 +0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,21 +1,21 @@
-INSERT INTO operators (name) VALUES (‘set’);
-INSERT INTO operators (name) VALUES (‘get’);
-INSERT INTO operators (name) VALUES (‘pull’);
-INSERT INTO operators (name) VALUES (‘push’);
-INSERT INTO operators (name) VALUES (‘read’);
+INSERT INTO operators (name) VALUES (`bet`);
+INSERT INTO operators (name) VALUES (`get`);
+INSERT INTO operators (name) VALUES (`pull`);
+INSERT INTO operators (name) VALUES (`push`);
+INSERT INTO operators (name) VALUES (`read`);
 
-INSERT INTO product_types (name) VALUES (‘buku’);
-INSERT INTO product_types (name) VALUES (‘pensil’);
-INSERT INTO product_types (name) VALUES (‘penghapus’);
+INSERT INTO product_types (name) VALUES (`buku`);
+INSERT INTO product_types (name) VALUES (`pensil`);
+INSERT INTO product_types (name) VALUES (`penghapus`);
 
 
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (1, 3, ‘001X’, ‘meja’, 1);
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (1, 3, ‘002X’, ‘kursi’, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (1, 3, `001X`, `meja`, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (1, 3, `002X`, `kursi`, 1);
 
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, ‘001Y’, ‘pensil’, 1);
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, ‘002Y’, ‘wrap’, 1);
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, ‘003Y’, ‘desk’, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, `001Y`, `pensil, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, `002Y`, `wrap`, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, `003Y`, `desk`, 1);
 
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, ‘001Z’, ‘Pemotong’, 1);
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, ‘002Z’, ‘pemangkas’, 1);
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, ‘003Z’, ‘penggabung’, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `001Z`, `Pemotong`, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `002Z`, `pemangkas`, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `003Z`, `penggabung`, 1);
