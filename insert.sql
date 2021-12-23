ff6df960e5229b6ec0e30b0c191d6f02ddc81aa5
latifis <74188153+latifis@users.noreply.github.com>
2021-12-23 12:14:07 +0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -16,6 +16,17 @@ INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2
 INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, `002Y`, `wrap`, 1);
 INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (2, 1, `003Y`, `desk`, 1);
 
-INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `001Z`, `Pemotong`, 1);
+INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `001Z`, `pemotong`, 1);
 INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `002Z`, `pemangkas`, 1);
 INSERT INTO product (product_type_id, operator_id, code, name, status) VALUES (3, 4, `003Z`, `penggabung`, 1);
+
+
+INSERT INTO product_descriptions (id, description) VALUES (1, `meja`);
+INSERT INTO product_descriptions (id, description) VALUES (2, `kursi`);
+INSERT INTO product_descriptions (id, description) VALUES (3, `pensil`);
+INSERT INTO product_descriptions (id, description) VALUES (4, `wrap`);
+INSERT INTO product_descriptions (id, description) VALUES (5, `desk`);
+INSERT INTO product_descriptions (id, description) VALUES (6, `pemotong`);
+INSERT INTO product_descriptions (id, description) VALUES (7, `pemangkas`);
+INSERT INTO product_descriptions (id, description) VALUES (8, `penggabung`);
+
