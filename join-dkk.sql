f77790f676cc0763d47ab22d21eb8addbbb7e4e0
latifis <74188153+latifis@users.noreply.github.com>
2021-12-23 18:10:30 +0700

    Update join-dkk.sql

--- join-dkk.sql
+++ join-dkk.sql
@@ -2,3 +2,6 @@ SELECT * FROM transaction WHERE user_id = 1 OR user_id = 2;
 
 SELECT SUM(total_price) as jumlah_harga FROM transaction WHERE user_id = 1;
 
+SELECT COUNT(td.transaction_id) FROM transaction_details td 
+INNER JOIN products p ON td.transaction_id = p.id
+WHERE td.transaction_id = 2
