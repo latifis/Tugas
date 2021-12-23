bc76e1600ffda6c8ddc6818dbf87b38623e9df51
latifis <74188153+latifis@users.noreply.github.com>
2021-12-23 13:17:37 +0700

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,7 +1,11 @@
-SELECT name FROM users WHERE gender = `l`
+SELECT name FROM users WHERE gender = `l`;
 
-SELECT * FROM products WHERE id = 3
+SELECT * FROM products WHERE id = 3;
 
-SELECT * FROM users WHERE create_at >= DATE(NOW()) - INTERVAL 7 DAY AND name LIKE `%a%`
+SELECT * FROM users WHERE create_at >= DATE(NOW()) - INTERVAL 7 DAY AND name LIKE `%a%`;
 
-SELECT COUNT(name) as jumlah_perempuan FROM users WHERE gender = `p`
+SELECT COUNT(name) as jumlah_perempuan FROM users WHERE gender = `p`;
+
+SELECT * FROM users ORDER BY name;
+
+SELECT * FROM products LIMIT 5;
