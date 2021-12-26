bbea3467499913aa63595d3e8c92d509ac8ee972
latifis <74188153+latifis@users.noreply.github.com>
2021-12-26 15:21:39 +0700

    Create Animals.java

---
+++ inhpolyan/Animals.java
@@ -0,0 +1,11 @@
+package com.bni.company.inhpolyan;
+
+public class Animals {
+    public String name;
+    public String feed;
+    public String gigi;
+
+    public void identity_myself(String name){
+        System.out.println("Hi I'm Parent of All Animal, My Name is " + name);
+    }
+}
